27c0ec11a7460827eb2ffd6f377706ccdc293ae8
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-31 15:09:42 +0200

    Update Game.java

--- src/com/gamerverse/model/Game.java
+++ src/com/gamerverse/model/Game.java
@@ -1,1 +1,41 @@
+package com.gamerverse.model;
+
+public class Game {
+    public String title;
+    private String genre;
+    protected double rating;
+
+    static int totalGames = 0;
+
+    public Game() {
+        this.title = "Untitled";
+        this.genre = "Unknown";
+        this.rating = 0.0;
+        totalGames++;
+    }
+
+    public Game(String title, String genre, double rating) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+        totalGames++;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void printInfo() {
+        System.out.println("Game: " + title + ", Genre: " + genre + ", Rating: " + rating);
+    }
+
+    public static void printTotalGames() {
+        System.out.println("Total games registered: " + totalGames);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Game " + title + " has been removed from memory.");
+    }
+}
 
